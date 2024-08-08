9f55cb66a3ddfd16b806fcd686b7ffc76b74f25b
James Munns <james@onevariable.com>
2024-08-08 12:24:01 -0500

    Add podcast transcript support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use rss::{
-    extension::itunes::{ITunesCategory, ITunesChannelExtension, ITunesItemExtension, ITunesOwner},
+    extension::{
+        itunes::{ITunesCategory, ITunesChannelExtension, ITunesItemExtension, ITunesOwner},
+        Extension, ExtensionBuilder, ExtensionMap,
+    },
     Channel, ChannelBuilder, Enclosure, Guid, Image, Item, ItemBuilder,
 };
 use serde::{Deserialize, Serialize};
+use xml::{reader::ParserConfig, writer::EmitterConfig};
 
 use std::{
     collections::{BTreeMap, HashMap, HashSet},
@@ -22,9 +26,9 @@ struct Podcast {
     language: String,
     copyright: String,
     webmaster: String,
-    managing_editor: String, // TODO: kebab-case
+    managing_editor: String,
     formats: Vec<String>,
-    hosting_base_url: String, // TODO: kebab-case
+    hosting_base_url: String,
 
     keywords: Vec<String>,
     explicit: bool,
@@ -59,6 +63,10 @@ struct ItunesCategory {
     itunesu_category: String,
 }
 
+// TODO: Some of these are actually optional fields, we could be more permissive
+// here with many fields, at least for itunes.
+//
+// See https://help.apple.com/itc/podcasts_connect/#/itcb54353390
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case", deny_unknown_fields)]
 struct Episode {
@@ -70,6 +78,8 @@ struct Episode {
     duration: String,     // TODO: NaiveTime?
     publish_date: String, // TODO: NaiveDateTime/DateTime, kebab-case?
     keywords: Vec<String>,
+    length_bytes: usize,
+    transcript_url: Option<String>,
 }
 
 fn main() {
@@ -77,9 +87,6 @@ fn main() {
     let mut in_file = File::open("example.toml").unwrap();
     in_file.read_to_string(&mut buffer).unwrap();
     let podcast = toml::from_str::<Podcast>(&buffer).unwrap();
-
-    // println!("{:#?}", podcast);
-
     let xmls = generate_xmls(podcast).unwrap();
 
     for (format, data) in xmls.iter() {
@@ -109,7 +116,7 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
     itunes.set_categories(vec![itunes_category]);
     itunes.set_image(pod.logo.url.clone());
     itunes.set_explicit(Some(
-        if pod.explicit { "Explicit" } else { "Clean" }.to_string(),
+        if pod.explicit { "true" } else { "false" }.to_string(),
     ));
 
     itunes.set_owner(itunes_owner);
@@ -128,7 +135,10 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
         "itunes".into(),
         "http://www.itunes.com/dtds/podcast-1.0.dtd".into(),
     );
-    namespaces.insert("itunesu".into(), "http://www.itunesu.com/feed".into());
+    namespaces.insert(
+        "podcast".into(),
+        "https://podcastindex.org/namespace/1.0".into(),
+    );
 
     let mut image = Image::default();
     image.set_url(pod.logo.url.clone());
@@ -153,7 +163,6 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
 
     // Currently unused items
     //
-    // .extensions(todo!()) // TODO: This, maybe? Anything other than itunes?
     // .text_input(todo!())
     // .skip_hours(todo!())
     // .skip_days(todo!())
@@ -178,7 +187,7 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
         itunes_item.set_image(Some(pod.logo.url.clone()));
         itunes_item.set_duration(Some(episode.duration)); // lol
         itunes_item.set_explicit(Some(
-            if pod.explicit { "Explicit" } else { "Clean" }.to_string(),
+            if pod.explicit { "true" } else { "false" }.to_string(),
         ));
         itunes_item.set_summary(episode.description.clone());
         itunes_item.set_subtitle(episode.subtitle.clone());
@@ -191,19 +200,8 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
             .title(episode.title.clone())
             .description(episode.description.clone())
             .author(pod.author_email.clone()) // email
-            // .categories()
-            // .comments() // URL
-
             .pub_date(episode.publish_date.clone()) // RFC822
-            // .source() // This RSS feed?
-            // .content() // ?
-            .itunes_ext(itunes_item) // TODO
-            // .dublin_core_ext()
-            ;
-
-        // .link() // Do in format
-        // .enclosure() // Do in format?
-        // .guid() // Do in format?
+            .itunes_ext(itunes_item);
 
         let mut cur_set = base_set.clone();
 
@@ -229,6 +227,7 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
                     .to_string();
 
                     encl.set_mime_type(mime);
+                    encl.set_length(episode.length_bytes.to_string());
 
                     let mut this_item = base_item.clone();
                     this_item.link(episode.url.clone());
@@ -237,10 +236,38 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
                     this_item.guid(Some(guid));
                     cur_set.remove(ext);
 
-                    item_map
-                        .entry(ext.to_string())
-                        .or_default()
-                        .push(this_item.build());
+                    let mut item = this_item.build();
+
+                    if let Some(transcript) = episode.transcript_url.clone() {
+                        let xc_ext = transcript.split('.').last().unwrap();
+                        let xcript_kind = match xc_ext {
+                            "vtt" => "text/vtt",
+                            "srt" => "application/srt",
+                            "txt" => "text/plain",
+                            _ => panic!("Unknown transcript extension?"),
+                        }
+                        .to_string();
+
+                        // Build an extension...
+                        let mut extension = ExtensionBuilder::default();
+                        extension.name("podcast:transcript");
+                        let mut attrs = BTreeMap::new();
+                        attrs.insert("url".to_string(), transcript);
+                        attrs.insert("type".to_string(), xcript_kind);
+                        extension.attrs(attrs);
+
+                        // I don't really understand what any of the following items do, they
+                        // are required for setting the extension however. None of the names seem
+                        // to actually matter?
+                        let mut im = BTreeMap::<String, Vec<Extension>>::new();
+                        im.insert("ext:name".to_string(), vec![extension.build()]);
+                        let mut extension_map = ExtensionMap::default();
+                        extension_map.insert("podcast".to_string(), im);
+
+                        item.set_extensions(extension_map);
+                    }
+
+                    item_map.entry(ext.to_string()).or_default().push(item);
                 }
                 (true, false) => {
                     eprintln!("We've already added a file of format '{}' for episode '{}'. Skipping file '{}' with duplicate format.", ext, episode.title, file)
@@ -262,8 +289,6 @@ fn generate_xmls(pod: Podcast) -> Result<HashMap<String, Channel>, ()> {
     Ok(map)
 }
 
-use xml::{reader::ParserConfig, writer::EmitterConfig};
-
 // https://users.rust-lang.org/t/pretty-printing-xml/76372/3
 fn format_xml(src: &[u8]) -> Result<String, xml::reader::Error> {
     let mut dest = Vec::new();
@@ -273,7 +298,7 @@ fn format_xml(src: &[u8]) -> Result<String, xml::reader::Error> {
         .create_reader(src);
     let mut writer = EmitterConfig::new()
         .perform_indent(true)
-        .normalize_empty_elements(false)
+        .normalize_empty_elements(true)
         .autopad_comments(false)
         .create_writer(&mut dest);
     for event in reader {
